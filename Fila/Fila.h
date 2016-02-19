b23551c7ab93c9fd217c193fa2d8e03714647b00
Lucas Basseti <lucas_basseti@hotmail.com>
2016-02-19 01:15:51 -0200

    commit inicial

---
+++ Fila/Fila.h
@@ -0,0 +1,15 @@
+
+typedef struct Item item;
+
+struct Item {
+	int valor;
+	item *prox;
+};
+
+int static FIM;
+
+void criar(item*);
+int vazia(item*);
+void enfileirar(item*, int);
+void desenfileirar(item*);
+int primeiro(item*);
---
+++ Fila/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	cout<<"impressao "<<endl;
+	
+	system("pause");
+	return 0;
+}
